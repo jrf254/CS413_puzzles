f1e18262c625dd7ac9ecc9aa9783e32aa7688542
jtt48 <jtt48@nau.edu>
2015-02-13 22:23:28 -0600

    Fixed Funk O Matic

--- src/Main.hx
+++ src/Main.hx
@@ -97,8 +97,8 @@ You would have a slice but you don't like watermelons. You are much more a Cante
 Ring, ring, ring, ring, ring, ring, ring, bannan phone.");
 		rootSprite.addChild(phone);
 		
-		funco = new Items("funco", 50, 20.5, false, "It's a Juke Box.
-Probably could turn it on if you wanted to.");
+		funco = new Items("funco", 50, 20.5, false, "It's a Funk-O-Matic.
+'Puts the Funk back in your music, even if it was never there in the first place!' There is a slot for a CD in the machine.");
 		rootSprite.addChild(funco);
 		
 		mag = new Items("magazines", 100, 500.5, false, "It's a stack of Magazines.
